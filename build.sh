44a4985eb92b7dfda83b8edc3fd91abcae73b10e
Thomas Heller <thomas.m.heller@gmail.com>
2020-06-29 17:09:53 +0200

    add ligatures patch

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 set -e
 
 make clean
-rm -f *.orig *.rej
-git checkout config.def.h st.1 st.c st.h x.c 
+rm -f *.orig *.rej hb.c hb.h
+git checkout Makefile config.def.h config.mk st.1 st.c st.h win.h x.c
 
+patch <st-ligatures-20200430-0.8.3.diff
 patch <st-newterm-0.8.2.diff
 # patch <st-no_bold_colors-0.8.1.diff
 # patch <st-solarized-dark-0.7.diff
